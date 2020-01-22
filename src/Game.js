039ab50a4514431fb8e5f8b0079c4a4c013205b1
KuKuRammus <vladkamyshanov@gmail.com>
2020-01-22 18:33:05 +0300

    Add base main loop

--- src/Game.js
+++ src/Game.js
@@ -1,8 +1,10 @@
 class Game
 {
-
     // Canvas drawing 2D context
     canvasCtx = null;
+
+    currentTime = 0;
+    lastTime = 0;
 
     constructor() {}
 
@@ -15,6 +17,26 @@ class Game
 
         // Save reference to drawing context
         this.canvasCtx = canvas.getContext('2d')
+    }
+
+    tick() {
+        window.requestAnimationFrame(this.tick.bind(this));
+
+        this.currentTime = (new Date()).getTime();
+        const deltaTime = (this.currentTime - this.lastTime) / 1000;
+
+        this.update(deltaTime);
+        this.render(deltaTime);
+
+        this.lastTime = this.currentTime;
+    }
+
+    update(deltaTime) {
+        // TODO
+    }
+
+    render(deltaTime) {
+        // TODO
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import Game from './Game'
 
 const game = new Game();
 game.init(FIELD_WIDTH, FIELD_HEIGHT, document.querySelector('body'));
-
+game.tick();
